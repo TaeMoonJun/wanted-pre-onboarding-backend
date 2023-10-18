4308980d6949f9e112e24558f489c935c3c8f1f8
Taejun Moon <junall10022@gmail.com>
2023-10-18 21:04:12 +0900

    feat: Add Recruitment Service interface

---
+++ src/main/java/com/example/wantedpreonboardingbackend/service/RecruitmentService.java
@@ -0,0 +1,17 @@
+package com.example.wantedpreonboardingbackend.service;
+
+import com.example.wantedpreonboardingbackend.dto.RegisterRecruitmentRequest;
+import com.example.wantedpreonboardingbackend.dto.RecruitmentResponse;
+import com.example.wantedpreonboardingbackend.dto.UpdateRecruitmentRequest;
+
+import java.util.List;
+
+public interface RecruitmentService {
+    RecruitmentResponse createRecruitment(RegisterRecruitmentRequest registerRecruitmentRequest);
+
+    RecruitmentResponse updateRecruitment(Long recruitmentId, UpdateRecruitmentRequest updateRecruitmentRequest);
+
+    void deleteRecruitment(Long recruitmentId);
+
+    List<RecruitmentResponse> getRecruitments();
+}
