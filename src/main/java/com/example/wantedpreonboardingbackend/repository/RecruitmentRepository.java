52bc90ae121f34e2a2ab412d0b25946124e60b65
Taejun Moon <junall10022@gmail.com>
2023-10-18 21:53:29 +0900

    feat: Add find all recruitment by company id method

--- src/main/java/com/example/wantedpreonboardingbackend/repository/RecruitmentRepository.java
+++ src/main/java/com/example/wantedpreonboardingbackend/repository/RecruitmentRepository.java
@@ -3,5 +3,8 @@ package com.example.wantedpreonboardingbackend.repository;
 import com.example.wantedpreonboardingbackend.entity.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+    List<Recruitment> findAllByCompanyId(Long id);
 }
